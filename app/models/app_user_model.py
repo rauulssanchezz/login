7e94b5d6ba4a267465a03efef30af51800ccc8a3
raul <rauulssanchezz@gmail.com>
2025-08-20 10:31:43 +0200

    FEAT: user_model

---
+++ app/models/app_user_model.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr, field_validator
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+class UserModel(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+@field_validator('password')
+@classmethod
+def hash_password(cls, v):
+    return pwd_context.hash(v)
