cd6e3a8b94d1af3f92deb629456d4bd13b1debfb
raul <rauulssanchezz@gmail.com>
2025-08-20 10:09:44 +0200

    FEAT: config main to create tables

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.database import engine, Base
 
+@asynccontextmanager
+async def create_tables(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
 app = FastAPI(
-    title='Login'
+    title='Login',
+    lifespan = create_tables
 )
