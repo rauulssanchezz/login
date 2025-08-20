314b3b604de48149c77062668b99b6aaf2a0c83a
raul <rauulssanchezz@gmail.com>
2025-08-20 11:06:12 +0200

    add router

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.schemas.app_user_schema import AppUserSchema
+from app.routers.auth_router import router as auth_router
 
 @asynccontextmanager
 async def create_tables(app: FastAPI):
@@ -13,3 +14,5 @@ app = FastAPI(
     title='Login',
     lifespan = create_tables
 )
+
+app.include_router(auth_router)
