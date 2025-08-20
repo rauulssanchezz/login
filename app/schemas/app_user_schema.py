adf4b94d08e53f2784259e465e400fe8eee26c9d
raul <rauulssanchezz@gmail.com>
2025-08-20 10:50:43 +0200

    UPDATE: add token column

--- app/schemas/app_user_schema.py
+++ app/schemas/app_user_schema.py
@@ -9,3 +9,4 @@ class AppUserSchema(Base):
     name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    token = Column(String, nullable=True)
